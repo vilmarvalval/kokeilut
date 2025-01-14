ab81666388d9682ec1cb31ca90e5ecc95ae813b6
vilmarvalval <ville.valtasalo@outlook.com>
2025-01-14 15:33:09 +0200

    tehtävä 2.6 + ääkköset poistettu tiedostonimistä

---
+++ tehtava2.6.py
@@ -0,0 +1,12 @@
+import random
+
+KolmiRng1 = random.randint(0, 9)
+KolmiRng2 = random.randint(0, 9)
+KolmiRng3 = random.randint(0, 9)
+
+print("Uusi satunnainen koodi kolminumeroiseen numerolukkoosi on: "+str(KolmiRng1)+str(KolmiRng2)+str(KolmiRng3))
+
+NeliRng1 = random.randint(1, 6)
+NeliRng2 = random.randint(1, 6)
+NeliRng3 = random.randint(1, 6)
+NeliRng4 = random.randint(1, 6)
