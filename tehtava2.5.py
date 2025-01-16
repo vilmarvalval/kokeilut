7cf19b6d57a20b01dbd59df2e8be0b946ca3d631
vilmarvalval <ville.valtasalo@outlook.com>
2025-01-16 10:21:12 +0200

    tehtävä 2.5 kirotus virheet korjattu

--- tehtava2.5.py
+++ tehtava2.5.py
@@ -1,13 +1,13 @@
-leviksat = float(input("Anna leviksät: "))
+leiviskat = float(input("Anna leiviskät: "))
 naulat = float(input("Anna naulat: "))
 luodit = float(input("Anna luodit: "))
 
 #muunnetaan grammoiksi, 1 leviksa vastaa 20 naulaa, 1 naula 32 luotia ja 1 luoti 13.3 grammaa.
-leviksatG = float(leviksat*20*32*13.3)
+leiviskatG = float(leiviskat*20*32*13.3)
 naulatG = float(naulat*32*13.3)
 luoditG = float(luodit*13.3)
 
-kilot = int(((leviksatG+naulatG+luoditG)/1000))
-grammat = float(leviksatG+naulatG+luoditG-(kilot*1000))
+kilot = int(((leiviskatG+naulatG+luoditG)/1000))
+grammat = float(leiviskatG+naulatG+luoditG-(kilot*1000))
 
 print("Massa käyttäen metristä järjestelmää on: "+str(kilot)+f"kg, ja {grammat:.2f}g.")
