805122c4fd8c2040003c5311e6bbf574ed5873ee
vilmarvalval <ville.valtasalo@outlook.com>
2025-01-16 13:05:32 +0200

    parempi tiedostojen järjestely +3.4

---
+++ moduuli3/tehtava3.4.py
@@ -0,0 +1,6 @@
+vuosi =  float(input("Anna tarkistettava vuosi: "))
+
+if vuosi % 400 == 0 or (vuosi % 100 == 0 and vuosi % 4 != 0):
+    print("Vuosi on karkaus vuosi!")
+else:
+    print("Vuosi ei ole karkausvuosi.")
