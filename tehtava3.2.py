c52c2ae80228043aff5caecd6c4def409cc10964
vilmarvalval <ville.valtasalo@outlook.com>
2025-01-16 11:37:52 +0200

    tehtävä 3.2

---
+++ tehtava3.2.py
@@ -0,0 +1,11 @@
+hytti = input("Anna hyttiluokka: ")
+if hytti == "LUX" or hytti == "lux":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif hytti =="A" or hytti == "a":
+        print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hytti == "B" or hytti == "b":
+            print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hytti == "C" or hytti == "c":
+                print("C on ikkunaton hytti autokannen alapuolella.")
+else:
+                print(hytti+" ei ole hyttiluokka.")
