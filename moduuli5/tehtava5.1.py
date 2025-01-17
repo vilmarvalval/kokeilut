302ba7d13c0e81b3fd563584aaac9d28ed65f49d
vilmarvalval <ville.valtasalo@outlook.com>
2025-01-17 14:09:10 +0200

    tehtävä tehty

---
+++ moduuli5/tehtava5.1.py
@@ -0,0 +1,20 @@
+import random
+heitot=[]
+arvat=[]
+print("Anna arpakuutioiden määrä:")
+luku = int(input())
+print("Anna arpakuutioiden tahkojen määrä:")
+tahkot = int(input())
+
+while luku > 0:
+    kuutio = "Noppa "+str(luku)
+    arvat.append(kuutio)
+    luku = luku-1
+
+print(f"{len(arvat)}:n {tahkot}-tahkoisen arpakuution tulokset:")
+for heitto in arvat:
+    tulos = random.randint(1, tahkot)
+    heitot.append(tulos)
+
+
+print(heitot)
