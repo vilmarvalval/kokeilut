aab9d2b20f1a6dd2851540ac68d458b2595a7e09
vilmarvalval <ville.valtasalo@outlook.com>
2025-01-28 10:32:04 +0200

    korjaus sisennyksiin

--- moduuli3/tehtava3.2.py
+++ moduuli3/tehtava3.2.py
@@ -2,10 +2,10 @@ hytti = input("Anna hyttiluokka: ")
 if hytti == "LUX" or hytti == "lux":
     print("LUX on parvekkeellinen hytti yläkannella.")
 elif hytti =="A" or hytti == "a":
-        print("A on ikkunallinen hytti autokannen yläpuolella.")
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
 elif hytti == "B" or hytti == "b":
-            print("B on ikkunaton hytti autokannen yläpuolella.")
+    print("B on ikkunaton hytti autokannen yläpuolella.")
 elif hytti == "C" or hytti == "c":
-                print("C on ikkunaton hytti autokannen alapuolella.")
+    print("C on ikkunaton hytti autokannen alapuolella.")
 else:
-                print(hytti+" ei ole hyttiluokka.")
+    print(hytti+" ei ole hyttiluokka.")
