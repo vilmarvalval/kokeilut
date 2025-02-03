1a0a68539335e1c63b23fc41d1739660739a190a
vilmarvalval <ville.valtasalo@outlook.com>
2025-02-03 10:28:12 +0200

    lisätty tehtävänannon toteutumaton osa

--- moduuli4/tehtava4.5.py
+++ moduuli4/tehtava4.5.py
@@ -2,10 +2,18 @@ tunnus = "python"
 salis = "rules"
 KysyTunnus = input("Anna käyttäjätunnus: ")
 KysySalis = input("Anna salasana: ")
+kerrat = 0
 
 while tunnus != KysyTunnus and salis != KysySalis:
-    print("Pääsy evätty.")
+    if tunnus != KysyTunnus or salis != KysySalis:
+        print("Väärä salasana tai käyttäjätunnus.")
+        kerrat = kerrat+1
+    if kerrat >= 5:
+        break
     KysyTunnus = input("Anna käyttäjätunnus: ")
     KysySalis = input("Anna salasana: ")
 
-print("Tervetuloa")
+if tunnus == KysyTunnus and salis == KysySalis:
+    print("Tervetuloa")
+else:
+    print("Pääsy evätty.")
