9cd24e577db00b52869d8819107f6a542e0065b1
vilmarvalval <ville.valtasalo@outlook.com>
2025-02-04 15:32:50 +0200

    tehtävä tehty

--- moduuli7/tehtava7.1.py
+++ moduuli7/tehtava7.1.py
@@ -3,11 +3,14 @@ kuukaudet = ("joulukuu","tammikuu","helmikuu","maaliskuu","huhtikuu","toukokuu",
 print("Anna kuukausi:")
 kuukausi = input().lower()
 
-if kuukaudet.index(kuukausi) <= 2:
-    print("Kuukausi on talvella.")
-elif kuukaudet.index(kuukausi) <= 5:
-    print("Kuukausi on keväällä.")
-elif kuukaudet.index(kuukausi) <= 8:
-    print("Kuukausi on kesällä.")
+if kuukausi in kuukaudet:
+    if kuukaudet.index(kuukausi) <= 2:
+        print("Kuukausi on talvella.")
+    elif kuukaudet.index(kuukausi) <= 5:
+        print("Kuukausi on keväällä.")
+    elif kuukaudet.index(kuukausi) <= 8:
+        print("Kuukausi on kesällä.")
+    else:
+        print("Kuukausi on syksyllä.")
 else:
-    print("Kuukausi on syksyllä.")
+    print(f"Kuukausi {kuukausi} ei löytynyt kuukausista.")
