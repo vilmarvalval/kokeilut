7edbeb74811fda3a8f9433f07c857b73d259fb0d
vilmarvalval <ville.valtasalo@outlook.com>
2025-04-11 13:14:12 +0300

    yhtälön korjausta: yhtäsuuruus ja epäyhtäsuuruus merkit vaihettu päikseen

--- moduuli3/tehtava3.4.py
+++ moduuli3/tehtava3.4.py
@@ -1,6 +1,7 @@
 vuosi =  float(input("Anna tarkistettava vuosi: "))
 
-if vuosi % 400 == 0 or (vuosi % 100 == 0 and vuosi % 4 != 0):
+if vuosi % 400 == 0 or (vuosi % 100 != 0 and vuosi % 4 == 0):
     print("Vuosi on karkaus vuosi!")
 else:
     print("Vuosi ei ole karkausvuosi.")
+
